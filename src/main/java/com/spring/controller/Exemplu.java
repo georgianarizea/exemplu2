626edf6c32270686564f7e18bc904e6ba9d3db8d
eduard.ciobanu <eduard.ciobanu@vauban.ro>
2015-11-13 10:22:13 +0200

    am modificat ceva

--- src/main/java/com/spring/controller/Exemplu.java
+++ src/main/java/com/spring/controller/Exemplu.java
@@ -2,14 +2,17 @@ package com.spring.controller;
 
 public class Exemplu {
 public static void main(String args[]){
-	System.out.println("111");
-	System.out.println("222");
-	System.out.println("333");
-	System.out.println("444");
 	System.out.println("555");
+	System.out.println("444");
+	System.out.println("333");
+	System.out.println("222");
+	System.out.println("111");
+
 	if(2 == 3){
+		if(4==3) System.out.println("asdasd");
 		int x =2;
 		int y = 3;
+		int xy=5;
 		int s = x+y;
 		System.out.println(s);
 	}
