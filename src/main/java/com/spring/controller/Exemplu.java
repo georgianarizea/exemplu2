535a2b91d86962617584ce5ab08dc3600f619a2b
Georgiana Rizea <georgiana.rizea@yahoo.com>
2015-11-13 10:17:41 +0200

    modificare

---
+++ src/main/java/com/spring/controller/Exemplu.java
@@ -0,0 +1,17 @@
+package com.spring.controller;
+
+public class Exemplu {
+public static void main(String args[]){
+	System.out.println("111");
+	System.out.println("222");
+	System.out.println("333");
+	System.out.println("444");
+	System.out.println("555");
+	if(2 == 3){
+		int x =2;
+		int y = 3;
+		int s = x+y;
+		System.out.println(s);
+	}
+}
+}
