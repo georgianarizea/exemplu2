9d6d50dda3d9cad129c826de0edda52921c57083
Georgiana Rizea <georgiana.rizea@yahoo.com>
2015-11-13 10:25:01 +0200

    alta modificare

--- src/main/java/com/spring/controller/Exemplu.java
+++ src/main/java/com/spring/controller/Exemplu.java
@@ -2,14 +2,18 @@ package com.spring.controller;
 
 public class Exemplu {
 public static void main(String args[]){
-	System.out.println("111");
 	System.out.println("222");
+	System.out.println("111");	
 	System.out.println("333");
-	System.out.println("444");
 	System.out.println("555");
+	System.out.println("444");
+	
 	if(2 == 3){
-		int x =2;
-		int y = 3;
+		int x =3;
+		int y = 5;
+		if( x == y) {
+			//sfdsgfdh
+		}
 		int s = x+y;
 		System.out.println(s);
 	}
